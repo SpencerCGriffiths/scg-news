d4cbf530671d18e536d68d178c5a0735fa78d3d4
Spencer <s.clarkegriffiths@gmail.com>
2023-12-12 15:57:45 +0000

    updated component structure for commentCards

--- src/Pages.jsx/SingleArticle.jsx
+++ src/Pages.jsx/SingleArticle.jsx
@@ -1,9 +1,19 @@
-import ViewSingleArticle from "../Components/ViewSingleArticle"
+import ViewSingleArticle from "../Components/CommentCard"
 import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import { getArticleById, getCommentById } from "../Utils/Queries"
+import { useNavigate } from 'react-router-dom'
+import { DateConverter } from "../Utils/DateConverter"
+import CommentCard from "../Components/CommentCard"
 
 const SingleArticle = () => { 
+
+    let navigate = useNavigate()
+
+
+    const handleClick = () => { 
+        navigate(-1)
+    }
 
     const [singleArticle, setSingleArticle] = useState()
     const [comments, setComments] = useState()
@@ -32,9 +42,25 @@ if (isLoading) {
 } 
 
 if(singleArticle && comments) { 
-return <> 
-    <ViewSingleArticle singleArticle = {singleArticle} comments = {comments}/> 
-    </>
+return ( 
+<article id="singleArticle" className="headerMargin" key= {singleArticle.article_id}> 
+<span>{singleArticle.title}</span>
+<div>{singleArticle.topic}</div>
+<img src={singleArticle.article_img_url} width = "100%" height = "40%"/>
+<div>{singleArticle.body}</div>
+<div>by {singleArticle.author}</div>
+<div>posted {DateConverter(singleArticle.created_at)}</div>
+<span>{singleArticle.votes} votes</span>
+<div>{singleArticle.comment_count} comments</div>
+<ul> 
+    {comments.map((comment)=> { 
+        return <CommentCard comment = { comment } key= {comment.comment_id}/> 
+    })} 
+
+</ul>
+<button onClick={handleClick}>return</button>
+</article>
+)
 }
 } 
 export default SingleArticle
