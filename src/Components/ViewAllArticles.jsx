448d91c39caace02c93d05054dcd66550d4e92e3
Spencer <s.clarkegriffiths@gmail.com>
2023-12-12 11:46:51 +0000

    added link to View All so clicking the title, picture or see all comments opens single article view

--- src/Components/ViewAllArticles.jsx
+++ src/Components/ViewAllArticles.jsx
@@ -1,4 +1,5 @@
 import { DateConverter } from "../Utils/DateConverter"
+import { Link } from 'react-router-dom'
 
 const ViewAllArticles = (props) => { 
 
@@ -7,19 +8,25 @@ const { articles } = props
 return ( 
     <div className="articles"> 
      {articles.map((article) => { 
-return <article key = {article.article_id} id="singleArticle"> 
+return <article  id="singleArticle" key = {article.article_id} > 
+<Link className="Link" to={`/${article.article_id}`}>
             <h3 id="articleTitle"> {article.title} </h3>
-            <div>{article.topic}</div> 
+            
             <img src={article.article_img_url} id="articleImg"/> 
+            </Link>         
+            <div>{article.topic}</div> 
             <div>{DateConverter(article.created_at)}</div>
             <div> by {article.author}</div>
             <div>
             <span>{article.comment_count} comments</span>
             <button>likes</button>
             <button>Dislike</button>
+            <Link className="Link" to={`/${article.article_id}`} >
             <button>See Comments</button>
+            </Link>
             </div>
         </article>
+        
      })}
    </div> 
 )
